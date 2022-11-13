015a4b52614e74d02210f41d405169613bac15f8
MartinVesely <20s.veselyma@seznam.cz>
2022-11-13 19:31:17 +0100

    Dělá ní další sekce po navbaru

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#button').mouseenter(function(){
+    $(this).addClass('button-hover') & $(this).removeClass('header-buttons__button');
+    })
+
+    $('#button').mouseleave(function(){
+        $(this).removeClass('button-hover') & $(this).addClass('header-buttons__button');
+    })
+
+    $('#button-phone').mouseenter(function(){
+        $(this).addClass('button-hover') & $(this).removeClass('header-buttons__button') & $('#img').attr('src','./img/phone-hover.png');
+        })
+    
+        $('#button-phone').mouseleave(function(){
+            $(this).removeClass('button-hover') & $(this).addClass('header-buttons__button') & $('#img').attr('src','./img/phone.png');
+        })
+});
