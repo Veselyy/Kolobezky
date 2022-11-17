6c090b610939e47b2bc774f7afa2137bb8b8253a
MartinVesely <20s.veselyma@seznam.cz>
2022-11-17 16:37:58 +0100

    Udělání dalších dvou sekcí. lae neí dodělaný slider

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,18 @@ $(document).ready(function(){
         $('#button-phone').mouseleave(function(){
             $(this).removeClass('button-hover') & $(this).addClass('header-buttons__button') & $('#img').attr('src','./img/phone.png');
         })
+
+    $('.offer-slider').slick({
+        slidesToShow: 2,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 2500,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+        ]
+    });
 });
